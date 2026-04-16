31986c752ae1b283218ea58d261a6048cb702e72
Vijeeth C R Gonti <vijeethcrgonti@gmail.com>
2026-04-16 12:38:01 -0500

    fix: remove unused variable and wildcard import

--- glue/scripts/transform_orders.py
+++ glue/scripts/transform_orders.py
@@ -11,7 +11,7 @@ from datetime import datetime
 
 from awsglue.context import GlueContext
 from awsglue.job import Job
-from awsglue.transforms import *
+from awsglue.transforms import ApplyMapping
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from pyspark.sql import DataFrame
